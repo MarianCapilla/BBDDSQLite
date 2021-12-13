e1de217a0b52717ee5b3289c18c92140d06e521f
MARIAN\dell <mch.formacion@gmail.com>
2021-12-13 10:51:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/bbddsqlite/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.bbddsqlite
+
+import android.database.sqlite.SQLiteDatabase
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.bbddsqlite.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var amigosDBHelper: miSQLiteHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        amigosDBHelper = miSQLiteHelper(this)
+
+        binding.btGuardar.setOnClickListener {
+            if (binding.etNombre.text.isNotBlank() &&
+                binding.etEmail.text.isNotBlank()) {
+                amigosDBHelper.anyadirDato(binding.etNombre.text.toString(),
+                    binding.etEmail.text.toString())
+                binding.etNombre.text.clear()
+                binding.etEmail.text.clear()
+                Toast.makeText(this, "Guardado",
+                    Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "No se ha podido guardar",
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+        binding.btConsultar.setOnClickListener {
+            binding.tvConsulta.text = ""
+            val cursor = amigosDBHelper.mostrarDatos()
+            if (cursor!!.moveToFirst()) {
+                do {
+                    binding.tvConsulta.append(
+                        cursor.getInt(0).toString() + ": ")
+                    binding.tvConsulta.append(
+                        cursor.getString(1).toString()+ ", ")
+                    binding.tvConsulta.append(
+                        cursor.getString(2).toString() + "\n")
+                } while (cursor.moveToNext())
+            }
+
+        }
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/bbddsqlite/miSQLiteHelper.kt
@@ -0,0 +1,47 @@
+package com.example.bbddsqlite
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class miSQLiteHelper (context: Context) : SQLiteOpenHelper(
+    context, "amigos.db", null, 1) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val ordenCreacion =
+                "CREATE TABLE amigos " +
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT" +
+                ",nombre TEXT" +
+                ",email TEXT)"
+        db!!.execSQL(ordenCreacion)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?,
+                           oldVersion: Int, newVersion: Int) {
+        val ordenBorrado = "DROP TABLE IF EXISTS amigos"
+        db!!.execSQL(ordenBorrado)
+        onCreate(db)
+    }
+
+    fun anyadirDato(nombre: String, email: String) {
+        val datos = ContentValues()
+        datos.put("nombre", nombre)
+        datos.put("email", email)
+
+        val db = this.writableDatabase /*Abrimos la base de datos en modo escritura*/
+        db.insert("amigos", null, datos)
+        db.close()
+    }
+
+    fun mostrarDatos(): Cursor? {
+        val db : SQLiteDatabase = this.readableDatabase /*Abrimos la base de datos en modo lectura*/
+        val cursor = db.rawQuery(
+            "SELECT * FROM amigos",
+            null)
+        return cursor
+    }
+
+
+}
